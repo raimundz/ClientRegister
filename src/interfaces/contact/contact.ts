3f5382b81457bed276862ae2072ab2b437593e49
Raimundo Neto <103120878+raimundz@users.noreply.github.com>
2023-02-13 15:53:04 -0300

    Delete src directory

--- src/interfaces/contact/contact.ts
+++
@@ -1,6 +0,0 @@
-export interface IContacts {
-  email: string;
-  name: string;
-  tel: number;
-  userId: string;
-}
--- src/routes/session/session.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import sessionController from "../../controllers/session/session.controllers";
-
-const sessionRoutes = Router();
-
-sessionRoutes.post("", sessionController);
-
-export default sessionRoutes;
